1725a21a3a8b4fd97c470ee16e26301f21034bbf
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-02-09 16:54:23 +0530

    Calling API by async-await (bootstrap-grid layout)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Arry from "./components/Arry";
 import Emp from "./components/Emp";
 import User from "./components/User";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Post from "./components/Post";
 
 export default class App extends Component{
   /* Which function does react call : render() */
   render(){/* render must return something(JSX) */
-    return(
+    return (
       <div>
         {/* <Arry />
         <hr />
         <Emp /> */}
-        <User />
+        {/* <User /> */}
+        <Post />
       </div>
     )
   }
