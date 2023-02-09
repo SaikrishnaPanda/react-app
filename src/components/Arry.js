9e102105c4f7c3e25abd4ae97a92ae9e22f42468
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-02-09 12:28:33 +0530

    ass and desc is done in array

--- src/components/Arry.js
+++ src/components/Arry.js
@@ -17,8 +17,27 @@ export default class Arry extends Component{
                         <li key={this.i++}>{e}</li>
                     ))
                 }
+                <br/>
+                <button onClick={()=>(this.sortArry('ASC'))}>ASC</button>
+                <button onClick={()=>(this.sortArry('DESC'))}>DESC</button>
             </div>
-        )
+        );
+    }
+    sortArry(dirrection){
+        let temp = [];
+        switch(dirrection){
+            case 'ASC':
+                temp = this.state.nums.sort((a, b) => a - b)
+                break;
+            case 'DESC':
+                temp = this.state.nums.sort((a, b) => b - a)
+                break;
+            default:
+                break;
+        }
+        this.setState(
+            {nums: temp}
+        );
     }
 }
 //arrow (=>) function
