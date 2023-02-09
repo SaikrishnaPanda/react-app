9042c9e49f49f2d4ff970c5572f9031416964c24
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-02-09 15:13:47 +0530

    calling API also added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Component } from "react";
 import Arry from "./components/Arry";
 import Emp from "./components/Emp";
+import User from "./components/User";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component{
   /* Which function does react call : render() */
   render(){/* render must return something(JSX) */
     return(
       <div>
-        <Arry />
+        {/* <Arry />
         <hr />
-        <Emp />
+        <Emp /> */}
+        <User />
       </div>
     )
   }
